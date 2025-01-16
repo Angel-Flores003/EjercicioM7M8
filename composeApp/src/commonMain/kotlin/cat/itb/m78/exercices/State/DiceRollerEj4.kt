db2db638423068e9160ac9671333e6e4e032b568
angel-abraham.flores.7e8 <angel-abraham.flores.7e8@itb.cat>
2025-01-16 10:39:52 +0100

    a

---
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/State/DiceRollerEj4.kt
@@ -0,0 +1,90 @@
+package cat.itb.m78.exercices.State
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
+import m78exercices.composeapp.generated.resources.Res
+import m78exercices.composeapp.generated.resources.dice_1
+import m78exercices.composeapp.generated.resources.generatedFace
+import m78exercices.composeapp.generated.resources.tapestry
+import m78exercices.composeapp.generated.resources.title
+import org.jetbrains.compose.resources.painterResource
+
+@Composable
+fun DiceRoller()
+{
+    val painter = painterResource(Res.drawable.tapestry)
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .paint(
+                painter,
+                contentScale = ContentScale.FillBounds
+            ),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Image(
+            painter = painterResource(Res.drawable.title),
+            modifier = Modifier.fillMaxWidth(),
+            contentDescription = null
+        )
+        val scope = rememberCoroutineScope()
+        val snackbarHostState = remember { SnackbarHostState() }
+        Button(onClick = {
+            //if ()
+                scope.launch {
+                    snackbarHostState.showSnackbar("JACKPOT!")
+                }
+            /*Scaffold(
+                snackbarHost = {
+                    SnackbarHost(hostState = snackbarHostState)
+                },
+            ){ padding ->
+                Button(modifier = Modifier.padding(padding), onClick = {
+                    scope.launch {
+                        snackbarHostState.showSnackbar("Hello!")
+                    }
+                }){
+                    Text("Say hello")
+                }
+            }*/
+        }
+            , modifier = Modifier.fillMaxWidth()) {
+            Text("ROLL THE DICE")
+        }
+        Scaffold(
+            snackbarHost = {
+                SnackbarHost(hostState = snackbarHostState)
+            },
+        ){ padding ->
+            Button(modifier = Modifier.padding(padding), onClick = {
+                scope.launch {
+                    snackbarHostState.showSnackbar("JACKPOT!")
+                }
+            }){
+                Text("Say hello")
+            }
+        }
+    }
+}
--- composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/State/SecretNumberEj3.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/State/SecretNumberEj3.kt
@@ -2,43 +2,81 @@ package cat.itb.m78.exercices.State
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
+import kotlinx.coroutines.launch
+import kotlin.random.Random
 
 @Composable
-fun SecretNumber()
-{
-    Column (horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxWidth()){
-        Text("Endevina el número secret", fontWeight = FontWeight.Bold)
+fun SecretNumber() {
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        val secretNumber = remember { Random.nextInt(0, 101) }
         var text by remember { mutableStateOf("") }
+        var message by remember { mutableStateOf("") }
+        var attempts by remember { mutableStateOf(0) }
+        Text("Endevina el número secret", fontWeight = FontWeight.Bold)
+
         OutlinedTextField(text,
             label = { Text("") },
             onValueChange = {
                 text = it
             })
-        var showDialog by remember { mutableStateOf(false) }
-        if (showDialog)
-            AlertDialog(
-                onDismissRequest={showDialog = false},
-                confirmButton={},
-                text = {Text("HELLO " + text)}
-            )
-        Button(onClick = { showDialog = true }) {
+        var TextIntent by remember { mutableStateOf(false) }
+        Button(
+            onClick = {
+                val userInput = text.toIntOrNull()
+                if (userInput == null) {
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar("Si us plau, introdueix un número vàlid.")
+                    }
+                } else {
+                    attempts++
+                    when {
+                        userInput < secretNumber -> {
+                            message = "El número que busques és més gran"
+                        }
+                        userInput > secretNumber -> {
+                            message = "El número que busques és més petit"
+                        }
+                        else -> {
+                            message = "Has encertat! El número era $secretNumber"
+                        }
+                    }
+
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar(message)
+                    }
+                }
+            },
+            modifier = Modifier.padding(top = 16.dp)
+        ) {
             Text("Validar")
         }
-        var textI by remember{ mutableStateOf("Intents: 0" ) }
-        Text(text = textI)
+
+        // Mostrar els intents realitzats
+        Text("Intents: $attempts", modifier = Modifier.padding(top = 16.dp))
+        Text(message)
     }
 }
