a0f82c705468ab96c0af7f1b0354bc58ae3a4e35
angel-abraham.flores.7e8 <angel-abraham.flores.7e8@itb.cat>
2025-03-11 10:19:59 +0100

    11/03/2025

--- composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/Api/Jokes.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/Api/Jokes.kt
@@ -1,9 +1,11 @@
 package cat.itb.m78.exercices.Api
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,6 +15,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
@@ -21,6 +24,10 @@ import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
+import m78exercices.composeapp.generated.resources.Res
+import m78exercices.composeapp.generated.resources.dice_2
+import m78exercices.composeapp.generated.resources.generatedFace
+import org.jetbrains.compose.resources.painterResource
 import kotlin.random.Random
 
 @Serializable
@@ -59,6 +66,11 @@ fun ChisteRandom() {
             Text("Punchline: ${joke.punchline}")
         } else {
             Text("Cargando chiste...")
+            Image(
+                painter = painterResource(Res.drawable.dice_2),
+                modifier = Modifier.size((100.dp)),
+                contentDescription = null
+            )
         }
     }
 }
