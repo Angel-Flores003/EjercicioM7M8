5abbba03cdb011d614b94ef1413fce650d8a19e3
angel-abraham.flores.7e8 <angel-abraham.flores.7e8@itb.cat>
2025-04-01 10:45:56 +0200

    01/04/2025

--- composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/App.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/App.kt
@@ -4,10 +4,11 @@ import androidx.compose.runtime.*
 import cat.itb.m78.exercices.Api.Country.countriescreen
 import cat.itb.m78.exercices.Api.CountryMateu.CountriesScreen
 import cat.itb.m78.exercices.Api.Estatembassaments.EmbassamentsScreen
+import cat.itb.m78.exercices.Projectwireframe.WireframeDetailScreen
 import cat.itb.m78.exercices.Projectwireframe.WireframeMainScreen
 import cat.itb.m78.exercices.theme.AppTheme
 
 @Composable
 internal fun App() = AppTheme {
-    WireframeMainScreen()
+    WireframeDetailScreen()
 }
--- composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/Projectwireframe/WireframeDetailScreen.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/Projectwireframe/WireframeDetailScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -16,25 +17,36 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import coil3.compose.AsyncImage
 
 @Composable
-fun WireframeMainScreen() {
+fun WireframeDetailScreen() { //wireframeId: Long
     val viewModel = viewModel { WireframeViewModel() }
     val wireframe = viewModel.wireframe.value
-    WireframeScreen(wireframe)
+    WireframeDetailScreen(wireframe)
 }
 
 @Composable
-fun WireframeScreen(wireframe: List<Wireframe>?) {
-    if(wireframe==null){
+fun WireframeDetailScreen(wireframe: List<Wireframe>?) {
+    if (wireframe == null) {
         CircularProgressIndicator()
     } else {
-        LazyColumn() {
+        LazyColumn {
             items(wireframe) { wireframe ->
                 Row {
-                    Column(modifier = Modifier.weight(1f)) {
+                    Column(
+                        modifier = Modifier.weight(1f),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
                         Text("Name: " + wireframe.name, fontWeight = FontWeight.Bold)
+                        Text("Size: " + wireframe.size)
+                        Text("Age: " + wireframe.age)
                         wireframe.funcion?.let { Text("Post: " + it) }
                         Text("Bounty: " + wireframe.bounty.toString() + "$")
+                        Spacer(modifier = Modifier.height(10.dp))
+
                         wireframe.crew?.let { Text("Belongs to the crew: " + it.name) }
+                        wireframe.crew?.let { Text("Number of crew members: " + it.tripulantes) }
+                        wireframe.crew?.let { Text("Total price: " + it.total_price) }
+                        Spacer(modifier = Modifier.height(10.dp))
+
                         wireframe.fruit?.let { Text("Fruit: " + it.name) }
                         wireframe.fruit?.let {
                             AsyncImage(
@@ -42,7 +54,12 @@ fun WireframeScreen(wireframe: List<Wireframe>?) {
                                 contentDescription = null
                             )
                         }
+                        wireframe.fruit?.let { Text("Roman name: " + it.roman_name) }
+                        wireframe.fruit?.let { Text("Type: " + it.type) }
+                        wireframe.fruit?.let { Text("Description: " + it.description) }
                         Spacer(modifier = Modifier.height(15.dp))
+                        Text("Status: " + wireframe.status)
+                        Spacer(modifier = Modifier.height(25.dp))
                     }
                 }
             }
