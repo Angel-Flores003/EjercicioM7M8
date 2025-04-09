6a0cafc8349ff7913c246c360cec2eac28f054ea
angel-abraham.flores.7e8 <angel-abraham.flores.7e8@itb.cat>
2025-04-09 13:00:49 +0200

    09/04/2025

--- composeApp/src/androidMain/kotlin/cat/itb/m78/exercices/Ejemplo/CamerScreen.kt
+++ composeApp/src/androidMain/kotlin/cat/itb/m78/exercices/Ejemplo/CamerScreen.kt
@@ -3,12 +3,15 @@ package cat.itb.m78.exercices.Ejemplo
 import androidx.camera.compose.CameraXViewfinder
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
 
@@ -28,7 +31,8 @@ fun CameraScreen() {
                 surfaceRequest = request,
                 modifier = Modifier.fillMaxSize()
             )
-            Button({ takePhoto(context, imageCaptureUseCase) }) {
+            Button({ takePhoto(context, imageCaptureUseCase) },
+                modifier = Modifier.align(Alignment.BottomCenter).padding(50.dp)) {
                 Text("Take Photo")
             }
         }
