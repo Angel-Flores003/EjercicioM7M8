8fb3c293cb72668df3900eb0701deb5028158e29
angel-abraham.flores.7e8 <angel-abraham.flores.7e8@itb.cat>
2025-05-06 10:45:31 +0200

    6/05/2025

--- composeApp/src/androidMain/kotlin/cat/itb/m78/exercices/MapCompose/P3_Project/MainScreen.kt
+++ composeApp/src/androidMain/kotlin/cat/itb/m78/exercices/MapCompose/P3_Project/MainScreen.kt
@@ -27,12 +27,6 @@ fun MainScreen() {
         onMapClick = { navController.navigate(ProjectScreens.Map) },
         onListClick = { navController.navigate(ProjectScreens.List) }
     ) {
-//        when (currentScreen) {
-//            "map" -> MapScreen()
-//            "list" -> ListScreen()
-//        }
-
-
         NavHost(navController = navController, startDestination = ProjectScreens.Map) {
             composable<ProjectScreens.Map> {
                 MapScreen(
@@ -47,32 +41,3 @@ fun MainScreen() {
         }
     }
 }
-
-//class ProjectViewModel1() : ViewModel() {
-//    var addMarkerState = mutableStateOf(false)
-//    val drawerMenuState = mutableStateOf(false)
-//    val listScreen = mutableStateOf(false)
-//    val mapScreen = mutableStateOf(false)
-//    val permissionState = mutableStateOf(false)
-//    val surferRequest = mutableStateOf<SurfaceRequest?>(null)
-//    val capturedImageUri = mutableStateOf<List<Uri?>>(emptyList())
-//    val imageCaptureUseCase: ImageCapture = ImageCapture.Builder().build()
-//
-//    private val cameraPreviewUseCase = Preview.Builder().build().apply {
-//        setSurfaceProvider { newSurfaceRequest ->
-//            surferRequest.value = newSurfaceRequest
-//        }
-//    }
-//
-//    suspend fun bindToCamera(appContext: Context, lifecycleOwner: LifecycleOwner) {
-//        val processCameraProvider = ProcessCameraProvider.awaitInstance(appContext)
-//        processCameraProvider.bindToLifecycle(
-//            lifecycleOwner, DEFAULT_BACK_CAMERA, cameraPreviewUseCase, imageCaptureUseCase
-//        )
-//        try {
-//            awaitCancellation()
-//        } finally {
-//            processCameraProvider.unbindAll()
-//        }
-//    }
-//}
